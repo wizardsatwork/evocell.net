91e778a4daf4df8ea8bfdea1b6e18f4efeaa2c78
Jascha <jascha@jaeh.at>
2019-04-20 04:59:30 +0200

    add meta files

---
+++ config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const HTML = require('@mspg/transpile-posthtml')
+const CSS = require('@mspg/transpile-stylus')
+// const JS = require('@mspg/transpile-babel')
+
+const year = new Date().getFullYear()
+
+module.exports = {
+  TRANSPILERS: {
+    HTML,
+    CSS,
+    // JS,
+  },
+  year,
+  IGNORE_EXTENSIONS: ['ai', 'psd', 'xcf'],
+  WEB_ROOT: '/',
+
+  OUT_DIR: path.join(process.cwd(), 'docs'),
+}
